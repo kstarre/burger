efec28a76837a6e629d24b47a47bb70d4af59b5d
Katie Starrett <starrett.k.e@gmail.com>
2017-08-08 01:57:11 -0400

    html build

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const bodyParser = require('body-parser');
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override ?_method=DELETE
+// Override ?_method=DELETE or PUT
 app.use(methodOverride("_method"));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
