2a5a013229cc8fbb577c36975dc1fb5d761d8940
Katie Starrett <starrett.k.e@gmail.com>
2017-08-08 16:12:43 -0400

    trying another way to get mysql on heroku

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ Promises.promisifyAll(require("mysql/lib/Pool").prototype);
 
 let connection = mysql.createConnection({
 	port: 3306,
-	host: 'us-cdbr-iron-east-05.cleardb.net',
-	user: 'bc1d68bfdd6766',
-	password: 'f8c07cfa',
-	database: 'heroku_86f9850b1300fc6'
+	host: 'localhost',
+	user: 'root',
+	password: 'headache',
+	database: 'burgers_db'
 });
 
 module.exports = connection;
