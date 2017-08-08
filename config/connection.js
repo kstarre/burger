22a4485b3efb553733f9d356609847844db2b55b
Katie Starrett <starrett.k.e@gmail.com>
2017-08-08 16:19:01 -0400

    jawsdb attempt

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql');
 const Promises = require('bluebird');
+let connection;
 
 Promises.promisifyAll(require("mysql/lib/Connection").prototype);
 Promises.promisifyAll(require("mysql/lib/Pool").prototype);
 
-let connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: 'headache',
-	database: 'burgers_db'
-});
-
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'headache',
+		database: 'burgers_db'
+	});
+};
+connection.connect();
 module.exports = connection;
