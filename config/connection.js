ba0e4538b0733b7d3c07b4798fe38d7c91f0d751
Katie Starrett <starrett.k.e@gmail.com>
2017-08-08 16:04:52 -0400

    attempt fix 3rd time

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ Promises.promisifyAll(require("mysql/lib/Pool").prototype);
 
 let connection = mysql.createConnection({
 	port: 3306,
-	host: env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
-	user: env('DB_USERNAME', 'bc1d68bfdd6766'),
-	password: env('DB_PASSWORD', 'f8c07cfa'),
-	database: env('DB_DATABASE', 'heroku_86f9850b1300fc6')
+	host: 'us-cdbr-iron-east-05.cleardb.net',
+	user: 'bc1d68bfdd6766',
+	password: 'f8c07cfa',
+	database: 'heroku_86f9850b1300fc6'
 });
 
 module.exports = connection;
