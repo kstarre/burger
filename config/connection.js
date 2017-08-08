9ef71ce5a912df1b717a01b8a44ac6e19d73288e
Katie Starrett <starrett.k.e@gmail.com>
2017-08-08 16:01:31 -0400

    attempt another bug fix

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-const password = require('./passwords');
 const Promises = require('bluebird');
 
 Promises.promisifyAll(require("mysql/lib/Connection").prototype);
@@ -7,10 +6,10 @@ Promises.promisifyAll(require("mysql/lib/Pool").prototype);
 
 let connection = mysql.createConnection({
 	port: 3306,
-	host: env('DB_HOST', password.host),
-	user: env('DB_USERNAME', password.user),
-	password: env('DB_PASSWORD', password.password),
-	database: env('DB_DATABASE', password.database)
+	host: env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+	user: env('DB_USERNAME', 'bc1d68bfdd6766'),
+	password: env('DB_PASSWORD', 'f8c07cfa'),
+	database: env('DB_DATABASE', 'heroku_86f9850b1300fc6')
 });
 
 module.exports = connection;
